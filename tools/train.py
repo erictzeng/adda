a582a0637aa44f19cdd344c7298863b3fb8509e8
Eric Tzeng <etzeng@eecs.berkeley.edu>
2017-02-27 21:47:09 -0500

    Update train script to use model-based preprocessing

--- tools/train.py
+++ tools/train.py
@@ -12,43 +12,6 @@ from tqdm import tqdm
 import adda
 
 
-def preprocessing(inputs, model_fn):
-    inputs = tf.cast(inputs, tf.float32)
-    if model_fn.default_image_size is not None:
-        size = model_fn.default_image_size
-        inputs = tf.image.resize_images(inputs, [size, size])
-    if model_fn.mean is not None:
-        inputs = inputs - tf.constant(model_fn.mean)
-    if model_fn.bgr:
-        inputs = inputs[:, :, [2, 1, 0]]
-    return inputs
-
-def remove_first_scope(name):
-    return '/'.join(name.split('/')[1:])
-
-def collect_vars(scope, start=None, end=None, prepend_scope=None):
-    vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
-    var_dict = OrderedDict()
-    if isinstance(start, str):
-        for i, var in enumerate(vars):
-            var_name = remove_first_scope(var.op.name)
-            if var_name.startswith(start):
-                start = i
-                break
-    if isinstance(end, str):
-        for i, var in enumerate(vars):
-            var_name = remove_first_scope(var.op.name)
-            if var_name.startswith(end):
-                end = i
-                break
-    for var in vars[start:end]:
-        var_name = remove_first_scope(var.op.name)
-        if prepend_scope is not None:
-            var_name = os.path.join(prepend_scope, var_name)
-        var_dict[var_name] = var
-    return var_dict
-
-
 @click.command()
 @click.argument('dataset')
 @click.argument('split')
@@ -70,21 +33,19 @@ def main(dataset, split, model, output, gpu, iterations, batch_size, lr,
         logging.info('CUDA_VISIBLE_DEVICES specified, ignoring --gpu flag')
     else:
         os.environ['CUDA_VISIBLE_DEVICES'] = gpu
-        logging.info('Using GPU {}'.format(gpu))
+    logging.info('Using GPU {}'.format(os.environ['CUDA_VISIBLE_DEVICES']))
     dataset_name = dataset
     split_name = split
     dataset = getattr(adda.data.get_dataset(dataset), split)
     model_fn = adda.models.get_model_fn(model)
     im, label = dataset.tf_ops()
-    im = preprocessing(im, model_fn)
+    im = adda.models.preprocessing(im, model_fn)
     im_batch, label_batch = tf.train.batch([im, label], batch_size=batch_size)
     net, layers = model_fn(im_batch)
     if ignore_label is not None:
         mask = tf.not_equal(label_batch, ignore_label)
         label_batch = tf.boolean_mask(label_batch, mask)
         net = tf.boolean_mask(net, mask)
-    #loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        #logits=net, labels=label_batch)
     class_loss = tf.losses.sparse_softmax_cross_entropy(label_batch, net)
     loss = tf.losses.get_total_loss()
 
@@ -99,14 +60,14 @@ def main(dataset, split, model, output, gpu, iterations, batch_size, lr,
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     sess.run(tf.global_variables_initializer())
     if weights:
-        var_dict = collect_vars(model, end=weights_end)
+        var_dict = adda.util.collect_vars(model, end=weights_end)
         logging.info('Restoring weights from {}:'.format(weights))
         for src, tgt in var_dict.items():
             logging.info('    {:30} -> {:30}'.format(src, tgt.name))
         restorer = tf.train.Saver(var_list=var_dict)
         restorer.restore(sess, weights)
         
-    model_vars = collect_vars(model)
+    model_vars = adda.util.collect_vars(model)
     saver = tf.train.Saver(var_list=model_vars)
     output_dir = os.path.join('snapshot', output)
     if not os.path.exists(output_dir):
