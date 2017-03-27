686a209765406a812126cea38b487a389d975f66
Eric Tzeng <etzeng@eecs.berkeley.edu>
2017-03-27 16:44:34 -0700

    Add leaky parameter to adversarial discriminator

--- adda/adversary.py
+++ adda/adversary.py
@@ -5,13 +5,17 @@ import tflearn
 from tensorflow.contrib import slim
 
 
-def adversarial_discriminator(net, layers, scope='adversary', reuse=False):
+def adversarial_discriminator(net, layers, scope='adversary', leaky=False):
+    if leaky:
+        activation_fn = tflearn.activations.leaky_relu
+    else:
+        activation_fn = tf.nn.relu
     with ExitStack() as stack:
-        stack.enter_context(tf.variable_scope(scope, reuse=reuse))
+        stack.enter_context(tf.variable_scope(scope))
         stack.enter_context(
             slim.arg_scope(
                 [slim.fully_connected],
-                activation_fn=tflearn.activations.leaky_relu,
+                activation_fn=activation_fn,
                 weights_regularizer=slim.l2_regularizer(2.5e-5)))
         for dim in layers:
             net = slim.fully_connected(net, dim)
