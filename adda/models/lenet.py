825c7996fa655497d1d6db66c6ccabfcca24ba72
Eric Tzeng <etzeng@eecs.berkeley.edu>
2017-03-27 16:51:31 -0700

    Add scopes to lenet model definition

--- adda/models/lenet.py
+++ adda/models/lenet.py
@@ -19,18 +19,18 @@ def lenet(inputs, scope='lenet', is_training=True, reuse=False):
                     activation_fn=tf.nn.relu,
                     weights_regularizer=slim.l2_regularizer(2.5e-5)))
             stack.enter_context(slim.arg_scope([slim.conv2d], padding='VALID'))
-            net = slim.conv2d(net, 20, 5)
+            net = slim.conv2d(net, 20, 5, scope='conv1')
             layers['conv1'] = net
-            net = slim.max_pool2d(net, 2, stride=2)
+            net = slim.max_pool2d(net, 2, stride=2, scope='pool1')
             layers['pool1'] = net
-            net = slim.conv2d(net, 50, 5)
+            net = slim.conv2d(net, 50, 5, scope='conv2')
             layers['conv2'] = net
-            net = slim.max_pool2d(net, 2, stride=2)
+            net = slim.max_pool2d(net, 2, stride=2, scope='pool2')
             layers['pool2'] = net
             net = tf.contrib.layers.flatten(net)
-            net = slim.fully_connected(net, 500)
+            net = slim.fully_connected(net, 500, scope='fc3')
             layers['fc3'] = net
-            net = slim.fully_connected(net, 10, activation_fn=None)
+            net = slim.fully_connected(net, 10, activation_fn=None, scope='fc4')
             layers['fc4'] = net
     return net, layers
 lenet.default_image_size = 28
